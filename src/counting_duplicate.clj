b3cc30418c6676ff8a7b4357179242e98f0dd1d7
sungsik.kim <skystar819@gmail.com>
2024-10-15 08:16:22 +0900

    트리보나치 문제 코드 풀이 추가

--- src/counting_duplicate.clj
+++ src/counting_duplicate.clj
@@ -258,3 +258,23 @@
 ;; 다른 풀이
 (defn max-tri-sum [n]
   (->> n distinct sort (take-last 3) (reduce +)))
+
+;; Tribonacci Sequence
+(defn tribonacci [[a b c] n]
+  (loop [l 3 r [1 1 1]]
+    (cond
+      (= l n) r
+      (= n 0) []
+      (= n 1) [a]
+      (= n 2) [a b]
+      (= n 3) [a b c]
+      :else (recur (inc l) (conj r (apply + (take-last 3 r)))))))
+
+(tribonacci [1 1 1] 0)
+
+;; 다른 풀이
+(defn trib [a b c]
+  (lazy-seq
+   (cons a (trib b c (+ a b c)))))
+
+(take 3 (trib 1 1 1))
