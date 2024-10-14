e83a7fc3a9cc9a8b470b4319ad4a7d5ce0775fa3
sungsik.kim <skystar819@gmail.com>
2024-10-15 06:00:30 +0900

    다른 풀이 추가

--- src/counting_duplicate.clj
+++ src/counting_duplicate.clj
@@ -248,8 +248,13 @@
 
 (sort > (set [2 9 13 10 5 2 9 5]))
 
+;; Maximum Triplet Sum (Array Series #7)
 (defn get-triple-max [numbers]
   (loop [i 0 r [] d (sort > (set numbers))]
     (let [m (first d)]
       (if (= i 3) r
           (recur (inc i) (conj r m) (rest d))))))
+
+;; 다른 풀이
+(defn max-tri-sum [n]
+  (->> n distinct sort (take-last 3) (reduce +)))
