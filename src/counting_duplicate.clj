dd44759e67230dda80e84ee62036e8cd8446c207
sungsik.kim <skystar819@gmail.com>
2024-10-15 12:28:47 +0900

    solve A Rule of Divisibility by 7

--- src/counting_duplicate.clj
+++ src/counting_duplicate.clj
@@ -277,4 +277,28 @@
   (lazy-seq
    (cons a (trib b c (+ a b c)))))
 
-(take 3 (trib 1 1 1))
+(take 10 (trib 1 1 1))
+
+(- (count "477557101") 1)
+
+;; A Rule of Divisibility by 7
+(loop [s 0 v 234002979492340029794]
+  (let [sv (str v)]
+    (if (<= (count sv) 2) [v s]
+        (recur (inc s) (- (bigint (apply str (take (- (count sv) 1) sv)))
+                          (* 2 (Character/getNumericValue (last sv))))))))
+
+;; 다른 방법
+(defn seven [m]
+  (loop [m m steps 0]
+    (if (< m 100) ;; 100보다 작으면 2자리 이다.
+      [m, steps]
+      (let [x (quot m 10) ;; 나눴을 때의 값
+            y (rem m 10)] ;; 나머지 값
+        (recur (- x (* 2 y)) (inc steps))))))
+
+;; 나누기
+(quot 477557101 10)
+
+;; 나머지
+(rem 477557101 10)
