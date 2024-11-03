4ec7fb75d9fe0af0a37bef3994c123fa271064e9
sungsik.kim <skystar819@gmail.com>
2024-11-04 06:46:52 +0900

    solved categorize new member

---
+++ src/categorize_new_member.clj
@@ -0,0 +1,8 @@
+(ns categorize-new-member)
+
+(def data [[74 10] [55 6] [12 -2] [68 7]])
+
+(defn open-or-senior [data]
+  (map (fn [[a h]] (if
+                    (and (>= a 55) (> h 7)) "Senior"
+                    "Open")) data))
