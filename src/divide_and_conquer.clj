2e1487c0844a23ecf22f2a1aef4daf97a87cfa0e
sungsik.kim <skystar819@gmail.com>
2024-11-04 05:54:08 +0900

    solved divide and conquer

---
+++ src/divide_and_conquer.clj
@@ -0,0 +1,8 @@
+(ns divide-and-conquer)
+
+(def x ["5" "0" 9 3 2 1 "9" 6 7])
+
+
+(defn div-con [x]
+  (- (apply + (filter integer? x))
+     (apply + (map #(Integer/valueOf %) (filter string? x)))))
