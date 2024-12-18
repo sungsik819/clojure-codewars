4f4f4931b6ceffd670d68f6a71a8ec47d141c60c
sungsik.kim <skystar819@gmail.com>
2024-12-19 06:29:50 +0900

    composing squared strings 다른 사람 풀이법 추가

--- src/kyu_7/composing_squared_strings.clj
+++ src/kyu_7/composing_squared_strings.clj
@@ -4,7 +4,7 @@
 (def s1 "byGt\nhTts\nRTFF\nCnnI")
 (def s2 "jIRl\nViBu\nrWOb\nNkTB")
 
-;; 풀이 과ㅇ
+;; 풀이 과정
 (string/split s1 #"\n")
 (reverse (string/split s2 #"\n"))
 
@@ -33,3 +33,12 @@
                  (rest sr)
                  (inc fi)
                  (dec si))))))
+
+
+;; 다른 사람 풀이법
+;; take를 사용하여 str을 만드는 과정 보다는 subs를 사용하여 str을 만드는 과정이 단순한듯
+(defn compose1 [s1 s2]
+  (let [a2 (vec (reverse (string/split s2 #"\n")))
+        l (count a2)
+        a1 (map-indexed (fn [i itm] (str (subs itm 0 (inc i)) (subs (a2 i) 0 (- l i)))) (string/split s1 #"\n"))]
+    (string/join "\n" a1)))
