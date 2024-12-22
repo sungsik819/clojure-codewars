83f69fd7d71a28fa20784e05affafa401811121b
sungsik.kim <skystar819@gmail.com>
2024-12-23 05:29:07 +0900

    solved easy wallpaper

---
+++ src/kyu_7/easy_wallpaper.clj
@@ -0,0 +1,11 @@
+(ns kyu-7.easy-wallpaper)
+
+(def numbers
+  ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"])
+
+(def l 6.3)
+(def w 4.5)
+(def h 3.29)
+
+(if (zero? (* h w l)) (numbers 0)
+    (numbers (int (Math/ceil (/ (* (+ (* l h 2) (* w h 2)) 1.15) 5.2)))))
