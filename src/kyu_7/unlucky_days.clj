9911b96ec29266f3b3ab6002dd83c156c270a584
sungsik.kim <skystar819@gmail.com>
2024-12-25 07:03:37 +0900

    unlucky day 다른 방법 풀이 추가

--- src/kyu_7/unlucky_days.clj
+++ src/kyu_7/unlucky_days.clj
@@ -6,3 +6,7 @@
 (defn unluckyDays [year]
   (count (filter #(= DayOfWeek/FRIDAY (.getDayOfWeek %))
                  (map #(LocalDate/of year % 13) (range 1 13)))))
+
+;; filter만 사용하여 간소화
+(count (filter #(->> (.getDayOfWeek (LocalDate/of year % 13))
+                     (= DayOfWeek/FRIDAY)) (range 1 13)))
